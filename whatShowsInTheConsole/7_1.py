88b1e5b03c9ee37b69b95c0a4e4d20e8314f9806
skylite <zsolt.lengyel21@gmail.com>
2020-10-13 10:33:09 +0200

    10_13_2020_10_33_09 fast commit

--- whatShowsInTheConsole/7_1.py
+++ whatShowsInTheConsole/7_1.py
@@ -1,10 +1,16 @@
 vechicle = 'bus'
-alternative_vechicle = 'plane'
 
 
-def drive(v):
-    vechicle = list(vechicle)
-    print('driving a {}'.format(v))
+def drive():
+    # if you're assigning to "vechicle", it will only look for "vechicle" in
+    # local scope. Assigning to a name makes it local to that function. Since
+    # there is no such local variable at the point you're trying to use it, you
+    # get an error.
+
+    # because assignment says "this thing is local, only look for it in local
+    # scope".
+    vechicle = vechicle
+    print('driving a {}'.format(vechicle))
 
 
-drive(alternative_vechicle)
+drive()
